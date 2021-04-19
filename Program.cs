50e1f9b1c62c438c59f3065169e7a9d0e57163b1
ISMAYIL <azeri_ismayil@yahoo.com>
2021-04-19 10:43:46 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld__
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello Amil");
             Console.ReadKey();
 
         }
